7dd8ae6d038baf2f4c76fd0e3ca6c7a349fa7c66
Jascha Rafael Franke <hola@11once.de>
2018-08-08 19:20:25 +0200

    should improve my css skills

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 
 
 
-Restaurant.create(name: 'Lode and Stijn', address: 'Irgendwo in Kreuzberg', category: 'belgian')
-Restaurant.create(name: 'Nobelhart und Schmutzig', address: 'Irgendwo in Kreuzberg', category: 'belgian')
-Restaurant.create(name: 'Kuestlichkeiten', address: 'Irgendwo in Kreuzberg', category: 'belgian')
-Restaurant.create(name: 'Babel', address: 'Irgendwo in Kreuzberg', category: 'belgian')
-Restaurant.create(name: 'EIns44', address: 'Irgendwo in Kreuzberg', category: 'belgian')
+Restaurant.create(name: 'Lode and Stijn', address: 'Kreuzberger Str.13, 50339 Scheunhausen', category: 'belgian')
+Restaurant.create(name: 'Nobelhart und Schmutzig', address: 'Luemmelunder Str.93, 03293 Schwingen', category: 'belgian')
+Restaurant.create(name: 'Kuestlichkeiten', address: 'Suederhoefterweg 3d, 25826 SPO', category: 'belgian')
+Restaurant.create(name: 'Babel', address: 'Suederstraße 27, 25426 Heide', category: 'belgian')
+Restaurant.create(name: 'EIns44', address: 'Am Wasserturm 13, 50858 Köln', category: 'belgian')
